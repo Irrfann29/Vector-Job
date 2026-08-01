b63a0a1bdaa016fe893524e5f6ac405968d805a5
Irrfann29 <mrirfanhashmi2303@gmail.com>
2026-08-01 09:02:01 +0530

    DB setup

--- server.js
+++ server.js
@@ -1,2 +1,11 @@
-const express = require ('express')
+require ("dotenv").config()
+const app = require('./src/app')
 
+const connectDB = require('./src/config/database')
+
+connectDB();
+
+app.listen(3000,()=>{
+    console.log("server is running on port 3000");
+    
+})
